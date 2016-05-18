f03affc2e6ef720260d35c82e79431ac04013c4f
Drew Wyatt <drew.wyatt@bottlerocketstudios.com>
2016-05-17 20:17:57 -0500

    shit works yo.

--- src/counter/index.ts
+++ src/counter/index.ts
@@ -1,2 +1,26 @@
-import {createStore} from 'redux';
+import {createStore, Action} from 'redux';
 
+function counter(state: number, action: Action): number {
+    let newState: number;
+    switch(action.type) {
+        case 'INCREMENT':
+            newState = state + 1;
+            break;
+        case 'DECREMENT':
+            newState = state - 1;
+            break;
+        default:
+            newState = state || 0;
+            break;
+    }
+    
+    return newState;
+}
+
+const store = createStore(counter);
+
+console.log(store.getState());
+
+store.dispatch({ type: 'INCREMENT' });
+
+console.log(store.getState());
